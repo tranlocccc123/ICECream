c65d7a8c659c903f7821f309a271f57caf2b4b4a
tranlocccc123 <ttranloc2004@gmail.com>
2024-05-08 23:56:30 +0700

    first git

---
+++ ICECream/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ICECream.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
