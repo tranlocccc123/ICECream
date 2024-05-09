9e078b7ef093563bcd6926367d9b8ee6a5e85371
tranlocccc123 <ttranloc2004@gmail.com>
2024-05-09 16:40:50 +0700

    git commit

--- ICECream/obj/Debug/net8.0/ICECream.AssemblyInfo.cs
+++ ICECream/obj/Debug/net8.0/ICECream.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICECream")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65d7a8c659c903f7821f309a271f57caf2b4b4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICECream")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICECream")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
